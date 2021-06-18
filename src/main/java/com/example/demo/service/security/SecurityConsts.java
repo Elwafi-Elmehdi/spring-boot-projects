b4e156395550cb7a616ac8c839fc527949dd4475
Mehdi <elwafi.courrier99@gmail.com>
2021-06-18 23:49:44 +0100

    Added security Consts

---
+++ src/main/java/com/example/demo/service/security/SecurityConsts.java
@@ -0,0 +1,15 @@
+package com.example.demo.service.security;
+
+public class SecurityConsts {
+    public static final long EXPIRATION_TIME = 432_000_000; // JWT 5 Days
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String JWT_TOKEN_HEADER = "Jwt-Token";
+    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
+    public static final String AUTHORITIES = "authorities";
+    public static final String FORBIDDEN_MESSAGE = "You need to log to access this page";
+    public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to acess this page";
+    public static final String OPTION_HTTP_METHOD = "OPTION";
+    public static final String[] PUBLIC_URLS = {"/user/login","/user/register","/user/resetpassword/**","/user/image/**"};
+    public static final String COMPANY_LLC = "Company LLC";
+    public static final String COMPANY_LLC_ADMINISTRATON = "Company LLC administration";
+}
