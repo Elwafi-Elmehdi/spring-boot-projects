38fa8d540897a311a4eb52415ca0242478f09c87
Mehdi <elwafi.courrier99@gmail.com>
2021-06-20 01:23:18 +0100

    Added Costum 403 responce

---
+++ src/main/java/com/example/demo/service/security/utils/JWTAuthenticationEntryPoint.java
@@ -0,0 +1,33 @@
+package com.example.demo.service.security.utils;
+
+import com.example.demo.bean.HttpResponse;
+import com.example.demo.service.security.SecurityConsts;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+public class JWTAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+        HttpResponse httpResponse = new HttpResponse(
+                HttpStatus.FORBIDDEN.value(),
+                HttpStatus.FORBIDDEN,
+                HttpStatus.FORBIDDEN.getReasonPhrase()
+                ,SecurityConsts.FORBIDDEN_MESSAGE
+        );
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream,httpResponse);
+        outputStream.flush();
+    }
+}
