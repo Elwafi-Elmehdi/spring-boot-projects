b3480d2003d30339b8b8772a6314eb95d74b6a6e
Mehdi <elwafi.courrier99@gmail.com>
2021-06-20 14:23:27 +0100

    Added Security Config

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.example.demo.config;
+
+import com.example.demo.service.impls.UserServiceImp;
+import com.example.demo.service.security.SecurityConsts;
+import com.example.demo.service.security.utils.JWTAccessDenielHandler;
+import com.example.demo.service.security.utils.JWTAuthenticationEntryPoint;
+import com.example.demo.service.security.utils.JWTAuthorizationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private JWTAuthorizationFilter jwtAuthorizationFilter;
+    @Autowired
+    private JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    @Autowired
+    private JWTAccessDenielHandler jwtAccessDenielHandler;
+    @Autowired
+    private UserServiceImp userServiceImp;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       auth.userDetailsService(userServiceImp).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .cors()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests().antMatchers(SecurityConsts.PUBLIC_URLS).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().accessDeniedHandler(jwtAccessDenielHandler)
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and().addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception{
+        return super.authenticationManager();
+    }
+
+    @Bean
+    BCryptPasswordEncoder getbCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
--- src/main/java/com/example/demo/service/security/utils/JWTAccessDenielHandler.java
+++ src/main/java/com/example/demo/service/security/utils/JWTAccessDenielHandler.java
@@ -4,10 +4,11 @@ import com.example.demo.bean.HttpResponse;
 import com.example.demo.service.security.SecurityConsts;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -15,18 +16,17 @@ import java.io.OutputStream;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Component
-public class JWTAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
-
+public class JWTAccessDenielHandler implements AccessDeniedHandler {
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException e) throws IOException, ServletException {
         HttpResponse httpResponse = new HttpResponse(
-                HttpStatus.FORBIDDEN.value(),
-                HttpStatus.FORBIDDEN,
-                HttpStatus.FORBIDDEN.getReasonPhrase()
-                ,SecurityConsts.FORBIDDEN_MESSAGE
+                HttpStatus.UNAUTHORIZED.value(),
+                HttpStatus.UNAUTHORIZED,
+                HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                SecurityConsts.ACCESS_DENIED_MESSAGE
         );
         response.setContentType(APPLICATION_JSON_VALUE);
-        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setStatus( HttpStatus.UNAUTHORIZED.value());
         OutputStream outputStream = response.getOutputStream();
         ObjectMapper mapper = new ObjectMapper();
         mapper.writeValue(outputStream,httpResponse);
