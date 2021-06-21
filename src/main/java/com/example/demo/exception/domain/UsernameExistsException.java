9e78f9ebe930e5e51fe4bc2509628596c2132c51
Mehdi <elwafi.courrier99@gmail.com>
2021-06-21 23:13:19 +0100

    Added Exception Classes

---
+++ src/main/java/com/example/demo/exception/domain/UsernameExistsException.java
@@ -0,0 +1,7 @@
+package com.example.demo.exception.domain;
+
+public class UsernameExistsException extends Exception{
+    public UsernameExistsException(String message) {
+        super(message);
+    }
+}
