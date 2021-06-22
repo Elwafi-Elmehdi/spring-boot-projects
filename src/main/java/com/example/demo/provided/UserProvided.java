2e599ba32bf04b902f1558f2c354bc857dab2406
Mehdi <elwafi.courrier99@gmail.com>
2021-06-22 16:39:04 +0100

    Tested The register EndPoint

--- src/main/java/com/example/demo/provided/UserProvided.java
+++ src/main/java/com/example/demo/provided/UserProvided.java
@@ -1,21 +1,26 @@
 package com.example.demo.provided;
 
+import com.example.demo.bean.User;
 import com.example.demo.exception.domain.EmailExistsException;
 import com.example.demo.exception.domain.ExceptionHandling;
+import com.example.demo.service.UserService;
 import com.example.demo.service.impls.UserServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserProvided extends ExceptionHandling {
     @Autowired
-    private UserServiceImp userService;
-    @GetMapping("/")
-    public String Hello() throws EmailExistsException {
-//        return "Applicatin is perfectly working";
-    throw new EmailExistsException("This Email is Taken!");
+    private UserService userService;
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) throws Exception {
+        return userService.register(
+                user.getFirstName(),
+                user.getLastName(),
+                user.getUsername(),
+                user.getEmail()
+        );
     }
+    
 }
