4dd6b7d4b0dea2b5a5b6ac0ad4ddcfdafe255178
Mehdi <elwafi.courrier99@gmail.com>
2021-07-03 00:30:37 +0100

    Implemnter AuthFailure EventListener

--- src/main/java/com/example/demo/listener/AuthenticationFailureListener.java
+++ src/main/java/com/example/demo/listener/AuthenticationFailureListener.java
@@ -2,6 +2,8 @@ package com.example.demo.listener;
 
 import com.example.demo.service.LoginAttemptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,7 +11,12 @@ public class AuthenticationFailureListener {
     @Autowired
     private LoginAttemptService loginAttemptService;
 
-    public void onAuthFailure(){
-
+    @EventListener
+    public void onAuthFailure(AuthenticationFailureBadCredentialsEvent event){
+        Object principal = event.getAuthentication().getPrincipal();
+        if(principal instanceof String){
+            String username = (String) event.getAuthentication().getPrincipal();
+            loginAttemptService.addUserToLoginAttemptCache(username);
+        }
     }
 }
