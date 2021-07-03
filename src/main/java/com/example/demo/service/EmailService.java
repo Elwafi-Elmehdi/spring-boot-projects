d0558227d8da7ccc64a67ca941c426e50fa9b0ab
Mehdi <elwafi.courrier99@gmail.com>
2021-07-03 19:46:47 +0100

    Added Create Email

--- src/main/java/com/example/demo/service/EmailService.java
+++ src/main/java/com/example/demo/service/EmailService.java
@@ -1,16 +1,36 @@
 package com.example.demo.service;
 
+import com.example.demo.bean.User;
 import com.example.demo.consts.EmailConsts;
 import com.example.demo.consts.EmailConsts.*;
 import org.springframework.stereotype.Service;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
 import java.util.Properties;
 
 import static com.example.demo.consts.EmailConsts.*;
 
 @Service
 public class EmailService {
+
+
+    private Message createEmail(User user,String password) throws MessagingException {
+        Message message = new MimeMessage(getEmailSession());
+        message.setFrom(new InternetAddress(FROM_EMAIL));
+        message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(user.getEmail(),false));
+        message.setRecipients(Message.RecipientType.CC,InternetAddress.parse(CC_EMAIL,false));
+        message.setSubject(EMAIL_SUBJECT);
+        message.setText("Hello "+user.getFirstName()+"\n\n"+"Your New Account Password Is : "+password+"\n\n Support Team.");
+        message.setSentDate(new Date());
+        message.saveChanges();
+        return message;
+    }
 
     private Session getEmailSession() {
         Properties properties = System.getProperties();
