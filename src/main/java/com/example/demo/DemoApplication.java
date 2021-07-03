2ca1a3ecd133c8addaf30c79e1082b086df5d2c1
Mehdi <elwafi.courrier99@gmail.com>
2021-07-03 22:09:37 +0100

    File Conts

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import com.example.demo.consts.FileConsts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        new File(FileConsts.USER_FOLDER).mkdirs();
     }
 
 }
--- src/main/java/com/example/demo/consts/FileConsts.java
+++ src/main/java/com/example/demo/consts/FileConsts.java
@@ -1,4 +1,13 @@
 package com.example.demo.consts;
 
 public class FileConsts {
+    public static final String USER_IMAGE_PATH = "/user/image";
+    public static final String JPG_EXTENSION = "jpg";
+    public static final String USER_FOLDER = System.getProperty("user.home")+"/supportportal/user";
+    public static final String DIRECTORY_CREATED = "Created Directory For :";
+    public static final String FILE_SAVED_IN_FILE_SYSTEM = "Saved file in file system by name :";
+    public static final String DOT = ".";
+    public static final String SLASH = "/";
+    public static final String TEMP_PROFILE_IMG = "https://robohash.org";
+
 }
