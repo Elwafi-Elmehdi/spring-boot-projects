f6b0a06a002f0010d7177b498b06e6421bd97233
Mehdi <elwafi.courrier99@gmail.com>
2021-08-31 23:54:44 +0100

    Added category controller

--- blog-site/src/main/java/com/example/blog/bean/Category.java
+++ blog-site/src/main/java/com/example/blog/bean/Category.java
@@ -17,4 +17,31 @@ public class Category {
     private String title;
     @JsonView(ResponseBody.CategoryBase.class)
     private String content;
+
+    public Category() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
--- blog-site/src/main/java/com/example/blog/service/CategoryService.java
+++ blog-site/src/main/java/com/example/blog/service/CategoryService.java
@@ -1,8 +1,11 @@
 package com.example.blog.service;
 
+import com.example.blog.bean.Category;
 import com.example.blog.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
@@ -10,6 +13,13 @@ public class CategoryService {
     @Autowired
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
+    }
+    public int addCategory(Category category){
+        return 0;
+    }
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
     }
 
 }
