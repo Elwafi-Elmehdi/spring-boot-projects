b708349fe5e7719967c68209b3bddd5a2cab4a9b
Mehdi <elwafi.courrier99@gmail.com>
2021-08-31 23:02:01 +0100

    Added add Post function

--- blog-site/src/main/java/com/example/blog/service/PostService.java
+++ blog-site/src/main/java/com/example/blog/service/PostService.java
@@ -5,6 +5,7 @@ import com.example.blog.bean.Post;
 import java.util.List;
 
 public interface PostService {
+    public int addPost(Post post);
     public List<Post> findAll();
     public Post findByRef(String ref);
 }
--- blog-site/src/main/java/com/example/blog/service/implement/PostServiceImpl.java
+++ blog-site/src/main/java/com/example/blog/service/implement/PostServiceImpl.java
@@ -3,9 +3,13 @@ package com.example.blog.service.implement;
 import com.example.blog.bean.Post;
 import com.example.blog.repository.PostRepository;
 import com.example.blog.service.PostService;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -16,6 +20,26 @@ public class PostServiceImpl implements PostService {
     public PostServiceImpl(PostRepository postRepository) {
         this.postRepository = postRepository;
     }
+
+    @Override
+    public int addPost(Post post) {
+        if(post.getReference()!= null){
+            Post post1 = postRepository.findByReference(post.getReference());
+            if(post1 != null){
+                return 0;
+        }
+        }
+        else if(post.getTitle() == null || post.getSummary()==null){
+            return -1;
+        }
+        else {
+            post.setReference(genReference());
+            post.setCreatedAt(new Date());
+            post.setUpdatedAt(null);
+            postRepository.save(post);
+            return 1;
+        }
+    }
     @Override
     public List<Post> findAll() {
         return postRepository.findAll();
@@ -25,4 +49,9 @@ public class PostServiceImpl implements PostService {
     public Post findByRef(String ref) {
         return postRepository.findByReference(ref);
     }
+
+    private String genReference() {
+        return RandomStringUtils.random(8);
+    }
+
 }
