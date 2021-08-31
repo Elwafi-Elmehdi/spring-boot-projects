b8dcb5da9d2a243ed082a6a4b1fa796b0fb7cd86
Mehdi <elwafi.courrier99@gmail.com>
2021-08-31 21:44:34 +0100

    Added Post Bean

---
+++ blog-site/src/main/java/com/example/blog/bean/Post.java
@@ -0,0 +1,18 @@
+package com.example.blog.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String description;
+    private Date createdAt;
+
+}
