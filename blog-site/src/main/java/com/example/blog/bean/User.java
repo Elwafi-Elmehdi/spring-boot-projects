703d26dfb896488474093cc1b7eafbe08bf643e3
Mehdi <elwafi.courrier99@gmail.com>
2021-09-01 00:35:26 +0100

    Updated User bean 2

--- blog-site/src/main/java/com/example/blog/bean/User.java
+++ blog-site/src/main/java/com/example/blog/bean/User.java
@@ -17,4 +17,80 @@ public class User {
     private String registeredAt;
     private String lastLogin;
     private String bio;
+
+    public User() {
+    }
+
+    public User(String userId, String email, String password, String phone, String lastLogin, String bio) {
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.lastLogin = lastLogin;
+        this.bio = bio;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRegisteredAt() {
+        return registeredAt;
+    }
+
+    public void setRegisteredAt(String registeredAt) {
+        this.registeredAt = registeredAt;
+    }
+
+    public String getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(String lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
 }
--- blog-site/src/main/java/com/example/blog/consts/ResponseBody.java
+++ blog-site/src/main/java/com/example/blog/consts/ResponseBody.java
@@ -2,4 +2,5 @@ package com.example.blog.consts;
 
 public interface ResponseBody {
     public class CategoryBase {}
+    public class UserBase {}
 }
