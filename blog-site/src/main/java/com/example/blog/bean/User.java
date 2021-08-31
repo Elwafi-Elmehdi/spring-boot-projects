ade33d477615b9e154b8280205a04d8c3f605af0
Mehdi <elwafi.courrier99@gmail.com>
2021-09-01 00:33:15 +0100

    Updated User bean

--- blog-site/src/main/java/com/example/blog/bean/User.java
+++ blog-site/src/main/java/com/example/blog/bean/User.java
@@ -1,4 +1,20 @@
 package com.example.blog.bean;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String userId;
+    private String email;
+    private String password;
+    private String phone;
+    private String registeredAt;
+    private String lastLogin;
+    private String bio;
 }
