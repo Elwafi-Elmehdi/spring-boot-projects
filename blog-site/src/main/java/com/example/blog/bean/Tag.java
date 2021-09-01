d0c00872a74166ed9a71ee6bccee365f46c7250d
Mehdi <elwafi.courrier99@gmail.com>
2021-09-01 12:24:43 +0100

    Added Tag repo

--- blog-site/src/main/java/com/example/blog/bean/Tag.java
+++ blog-site/src/main/java/com/example/blog/bean/Tag.java
@@ -1,13 +1,50 @@
 package com.example.blog.bean;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.example.blog.consts.ResponseBody;
+import com.fasterxml.jackson.annotation.JsonView;
+
+import javax.persistence.*;
 
 @Entity
 public class Tag {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @JsonView(ResponseBody.TagBase.class)
+    @Column(unique = true)
+    private String title;
+    @JsonView(ResponseBody.TagBase.class)
+    private String content;
+
+    public Tag() {
+    }
+
+    public Tag(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
--- blog-site/src/main/java/com/example/blog/consts/ResponseBody.java
+++ blog-site/src/main/java/com/example/blog/consts/ResponseBody.java
@@ -3,4 +3,5 @@ package com.example.blog.consts;
 public interface ResponseBody {
     public class CategoryBase {}
     public class UserBase {}
+    public class TagBase{}
 }
