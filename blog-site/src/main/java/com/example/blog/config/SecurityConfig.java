a0d385adb5a38c0045a3e3aab2850007d4e7538c
Mehdi <elwafi.courrier99@gmail.com>
2021-09-01 17:47:01 +0100

    Updated Security Config

--- blog-site/src/main/java/com/example/blog/config/SecurityConfig.java
+++ blog-site/src/main/java/com/example/blog/config/SecurityConfig.java
@@ -1,15 +1,37 @@
 package com.example.blog.config;
 
+import com.example.blog.consts.Security;
+import com.example.blog.service.implement.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserServiceImpl userService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(getPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http.csrf().disable()
+               .cors()
+               .and()
+               .authorizeRequests().antMatchers(Security.PUBLIC_URLS).permitAll()
+               .anyRequest().authenticated();
+    }
 
     @Bean
     BCryptPasswordEncoder getPasswordEncoder() {
--- blog-site/src/main/java/com/example/blog/service/implement/UserServiceImpl.java
+++ blog-site/src/main/java/com/example/blog/service/implement/UserServiceImpl.java
@@ -5,6 +5,9 @@ import com.example.blog.repository.UserRepository;
 import com.example.blog.service.UserService;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +15,7 @@ import java.util.Date;
 import java.util.List;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
     private UserRepository userRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
     @Autowired
@@ -31,15 +34,17 @@ public class UserServiceImpl implements UserService {
         User userFound = userRepository.findByUsername(user.getUsername());
         if(user.getUsername() != null && userFound == null){
             User user1 = new User();
+            String encoded = bCryptPasswordEncoder.encode(user.getPassword());
             user1.setUsername(user.getUsername());
             user1.setBio(user.getBio());
             user1.setEmail(user.getEmail());
-            user1.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+            user1.setPassword(encoded);
             user1.setPhone(user.getPhone());
             user1.setRegisteredAt(new Date());
             user1.setUserId(RandomStringUtils.randomAlphanumeric(12));
             user1.setLastLogin(null);
-            return userRepository.save(user1);
+            userRepository.save(user1);
+            return user1;
         }
         return null;
     }
@@ -47,5 +52,18 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findUserByUsername(String username) {
         return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(s);
+        if(user == null){
+            throw new UsernameNotFoundException("Cannot find User with "+ s);
+        }else {
+            user.setLastLogin(new Date());
+            userRepository.save(user);
+            return user;
+        }
+
     }
 }
