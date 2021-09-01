af21f9f313f6be72883c92d086e7771d1eb21438
Mehdi <elwafi.courrier99@gmail.com>
2021-09-01 18:05:26 +0100

    Updated Error const

--- blog-site/src/main/java/com/example/blog/bean/HttpResponse.java
+++ blog-site/src/main/java/com/example/blog/bean/HttpResponse.java
@@ -1,4 +1,23 @@
 package com.example.blog.bean;
 
+import com.example.blog.consts.Security;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+
 public class HttpResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "MM-dd-yyyy hh:mm:ss",timezone = Security.timeZone)
+    private Date timeSamp;
+    private int httpStatusCode; // 200,100,400
+    private HttpStatus httpStatus;
+    private String reason; // Http Reason (Libelle d Http responce)
+    private String message; // Developper Msg
+    public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+        this.timeSamp = new Date();
+        this.httpStatusCode = httpStatusCode;
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+        this.message = message;
+    }
 }
--- blog-site/src/main/java/com/example/blog/consts/Error.java
+++ blog-site/src/main/java/com/example/blog/consts/Error.java
@@ -1,14 +1,21 @@
 package com.example.blog.consts;
 
 public class Error {
-    public static final String USERNAME_FOUND = "Username found!";
-    public static final String USERNAME_NOT_FOUND = "Username was not found!";
-    public static final String EMAIL_FOUND = "Email found!";
+    public static final String USERNAME_FOUND = "USERNAME FOUND";
+    public static final String USERNAME_NOT_FOUND = "USERNAME NOT FOUND";
+    public static final String EMAIL_FOUND = "EMAIL FOUND";
     public static final String EMAIL_NOT_FOUND = "Email was not found!";
-    public static final String FORBIDDEN_MESSAGE = "You need to log to access this page";
-    public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to acess this page";
-    public static final String SERVER_ERROR = "Error 500 Server error";
-    public static final String DATABASE_ERROR = "Conflicts in the database";
-    public static final String PAGE_NOT_FOUND = "Page not found error 404";
-    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
+    public static final String FORBIDDEN_MESSAGE = "LOGIN TO ACCESS TO THIS PAGE";
+    public static final String ACCESS_DENIED_MESSAGE = "PERMISSION DENIED";
+    public static final String SERVER_ERROR = "INTERNAL Server ERROR";
+    public static final String DATABASE_ERROR = "DATABASE ERROR";
+    public static final String PAGE_NOT_FOUND = "PAGE NOT FOUND";
+    public static final String TOKEN_CANNOT_BE_VERIFIED = "TOKEN CANNOT BE VERIFIED";
+    public static final String ACCOUNT_LOCKED = "YOUR ACCOUNT HAS BEEN LOCKED";
+    public static final String METHOD_IS_NOT_ALLOWED = "REQUEST METHOD IS NOT SUPPORTED FOR THIS ENDPINT, PLEASE SEND A '%s' REQUEST";
+    public static final String INTERNAL_SERVER_ERROR = "AN ERROR OCCUPED WHILE PROCESSING THE REQUEST";
+    public static final String INCORRECT_CREDENTIALS = "USERNAME/PASSWARD INCORRECT. PLEASE TRY AGAIN";
+    public static final String ACCOUNT_DISABLED = "YOUR ACCOUNT HAS BEEN DISABLED";
+    public static final String ERROR_PROCESSING_FILE = "ERROR OCCURRED WHILE PROCESSING FILE ";
+    public static final String NO_PERMISSIONS = "YOU DO NOT HAVE THE PERMISSION FOR THIS ENDPOINT";
 }
