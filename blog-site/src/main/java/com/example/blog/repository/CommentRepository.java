37428858d2b899ea3026c638e4b2b5f780ff614a
Mehdi <elwafi.courrier99@gmail.com>
2021-09-01 13:06:28 +0100

    Updated comment repo

--- blog-site/src/main/java/com/example/blog/repository/CommentRepository.java
+++ blog-site/src/main/java/com/example/blog/repository/CommentRepository.java
@@ -1,4 +1,11 @@
 package com.example.blog.repository;
 
-public interface CommentRepository {
+import com.example.blog.bean.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+    public Comment findByReference(String ref);
+
 }
