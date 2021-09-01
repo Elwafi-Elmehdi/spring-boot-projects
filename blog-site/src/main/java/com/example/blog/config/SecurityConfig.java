10239117e628572b387d5f1c64fc45e772e854d6
Mehdi <elwafi.courrier99@gmail.com>
2021-09-01 17:18:19 +0100

    Added Bcrypt encoder

--- blog-site/src/main/java/com/example/blog/config/SecurityConfig.java
+++ blog-site/src/main/java/com/example/blog/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package com.example.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    @Bean
+    BCryptPasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- blog-site/src/main/java/com/example/blog/service/implement/UserServiceImpl.java
+++ blog-site/src/main/java/com/example/blog/service/implement/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.blog.repository.UserRepository;
 import com.example.blog.service.UserService;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -13,9 +14,11 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
@@ -31,7 +34,7 @@ public class UserServiceImpl implements UserService {
             user1.setUsername(user.getUsername());
             user1.setBio(user.getBio());
             user1.setEmail(user.getEmail());
-            user1.setPassword(user.getPassword());
+            user1.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
             user1.setPhone(user.getPhone());
             user1.setRegisteredAt(new Date());
             user1.setUserId(RandomStringUtils.randomAlphanumeric(12));
