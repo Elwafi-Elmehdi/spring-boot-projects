3b9c396b185d6bf5deb5ff7610ef6d05a41d0717
Mehdi <elwafi.courrier99@gmail.com>
2021-09-02 22:48:35 +0100

    Added added http error handler

--- blog-site/src/main/java/com/example/blog/bean/HttpResponse.java
+++ blog-site/src/main/java/com/example/blog/bean/HttpResponse.java
@@ -3,6 +3,8 @@ package com.example.blog.bean;
 import com.example.blog.consts.Security;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import java.util.Date;
 
 public class HttpResponse {
@@ -15,7 +17,7 @@ public class HttpResponse {
     private int httpStatusCode; // 200,100,400
     private HttpStatus httpStatus;
     private String reason; // Http Reason (Libelle d Http responce)
-    private String message; // Developper Msg
+    private String message;// Developper Msg
 
     public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
         this.timeSamp = new Date();
@@ -23,5 +25,18 @@ public class HttpResponse {
         this.httpStatus = httpStatus;
         this.reason = reason;
         this.message = message;
+    }
+
+    public HttpResponse() {
+    }
+
+    public static ResponseEntity<HttpResponse> createResponse(HttpStatus httpStatus,String message){
+        HttpResponse httpResponse = new HttpResponse(
+                httpStatus.value(),
+                httpStatus,
+                httpStatus.getReasonPhrase().toUpperCase(),
+                message
+        );
+        return new ResponseEntity<>(httpResponse,httpStatus);
     }
 }
---
+++ blog-site/src/main/java/com/example/blog/handler/ErrorHandler.java
@@ -0,0 +1,8 @@
+package com.example.blog.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+}
